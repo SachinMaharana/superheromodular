e2ff12b89628f3c88443325d0a818c9cd9f58f3d
Sachin <sachin.nicky@gmail.com>
2016-07-26 21:41:43 +0530

    Added public functions of storage module

---
+++ superstorage.py
@@ -0,0 +1,27 @@
+def set_superheros(superheros):
+	pass
+
+def superheros():
+	pass
+
+def set_locations(locations):
+	pass
+
+def locations():
+	pass
+
+def items():
+	pass
+
+def add_superhero(name, location):
+	pass
+
+def remove_superhero(name, location):
+	pass
+
+def init():
+	pass
+
+
+
+
